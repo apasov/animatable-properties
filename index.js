2352340f64cee2842e02413cfb52d193480138bf
Ilya Apasov <apasov@users.noreply.github.com>
2020-01-18 17:51:52 +0200

    Drop default export

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 import arrays from "./arrays"
 import functions from "./functions"
 
-export default {
-    properties: arrays.properties,
-    propertiesCSS: arrays.propertiesCSS,
-    propertiesJS: arrays.propertiesJS,
-    cssToJs: functions.cssToJs,
-    isAnimatable: functions.isAnimatable
-}
-
 export const properties = arrays.properties
 export const propertiesCSS = arrays.propertiesCSS
 export const propertiesJS = arrays.propertiesJS
