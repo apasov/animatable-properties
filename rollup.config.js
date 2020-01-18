fbae4c9c9132e41eead9307b402f73c7ce59bf06
Ilya Apasov <apasov@users.noreply.github.com>
2020-01-18 17:52:46 +0200

    Add compiled bundle for use in browsers

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from "@rollup/plugin-commonjs"
+import { terser } from 'rollup-plugin-terser'
+import * as pack from './package.json'
+
+const name = 'animatable'
+
+export default {
+    input: 'index.js',
+    output: {
+        file: `dist/${name}.js`,
+        format: 'iife',
+        name: name,
+        sourcemap: true
+    },
+    plugins: [resolve(), commonjs(), terser({
+        output: {
+            preamble: `//${pack.name} v${pack.version} ${pack.repository}`
+        }
+    })]
+}
