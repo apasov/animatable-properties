e51f6afe63c30f80b9553be8a71ae62abf0989ff
Ilya Apasov <apasov@users.noreply.github.com>
2020-04-09 19:45:25 +0200

    feat: add jsToCss() as alias to isAnimatable(string, true)

--- src/index.js
+++ src/index.js
@@ -5,4 +5,7 @@ export const properties = arrays.properties
 export const propertiesCSS = arrays.propertiesCSS
 export const propertiesJS = arrays.propertiesJS
 export const cssToJs = functions.cssToJs
+export const jsToCss = (value) => {
+  return functions.isAnimatable(value, true)
+}
 export const isAnimatable = functions.isAnimatable
